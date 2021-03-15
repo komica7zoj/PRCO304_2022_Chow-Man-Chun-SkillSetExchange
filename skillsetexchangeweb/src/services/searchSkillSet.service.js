fd467f3ba5c415062b15cd92d6a960117e3f8b99
chun <chun@DESKTOP-63O0AC1>
2021-03-15 17:49:37 +0800

    v5.1

--- skillsetexchangeweb/src/services/searchSkillSet.service.js
+++ skillsetexchangeweb/src/services/searchSkillSet.service.js
@@ -6,16 +6,12 @@ const API_URL = Search_API_URL;
 
 class SearchSkillSetService {
   getSkillSetFindResult(searchConditionDAO) {
- 
-///const form_data = new FormData();
-
-//form_data.append('searchConditionDAO',JSON.stringify(dao))
-
   return axios.post( API_URL + 'findskillset',{searchConditionDAO},{headers: authHeader()});
-
-//return axios.get( API_URL + 'findskillset', {data:{searchConditionDAO: dao}}, {headers: authHeader()});
-    //return axios.get( API_URL + 'findskillset',{params:{searchConditionDAO: JSON.stringify(dao)}},  {headers: authHeader()});
   }
+ 
+  getSkillSetInfoMatchResult(searchConditionDAO) {
+  return axios.post( API_URL + 'getSkillSetList',{searchConditionDAO},{headers: authHeader()});
+  } 
   
   getSkillSetPool() {
     return axios.get(API_URL + 'getskillsetpool', { headers: authHeader() });
